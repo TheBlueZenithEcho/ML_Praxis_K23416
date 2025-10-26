01db31e74d0d3c4127d7131a367e5c30ee9679bc
MyDuyn <lenguyenmyduyen035@gmail.com>
2025-10-26 13:33:17 +0700

    frontend setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
+
 function App() {
   return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Hello Praxis with Tailwind CSS!
-      </h1>
-    </div>
+    <>
+    </>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,47 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports = {
+    darkMode: ["class"],
+    content: [
+        "./pages/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./app/**/*.{ts,tsx}",
+        "./src/**/*.{ts,tsx}",
+        "node_modules/preline/dist/*.js"
+    ],
+    prefix: "",
+    theme: {
+        extend: {
+            colors: {
+                primary: '#2B7516',
+                secondary: '#082503',
+            },
 
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: '1rem',
+                    sm: '2rem',
+                    lg: '4rem',
+                    xl: '5rem',
+                    '2xl': '6rem',
+                }
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+                lora: ["lora"],
+                serif_text: ["DM Serif Text"],
+                inter: ["Inter", "sans-serif"]
+            },
+            screens: {
+                sm: '640px',
+                md: '760px',
+                lg: '960px',
+                xl: '1200px'
+            },
+            backgroundImage: {
+                'hero': "url('/src/assets/img/hero/bg.png')",
+            },
+        },
+    },
+    plugins: [],
+};
