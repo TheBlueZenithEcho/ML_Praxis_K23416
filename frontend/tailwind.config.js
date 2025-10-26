ee54e597f215f857a230275f7950ae608e9b33fc
Yennanng <yennan275@gmail.com>
2025-10-26 15:03:03 +0700

    app index

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,47 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
-        "./pages/**/*.{ts,tsx}",
-        "./components/**/*.{ts,tsx}",
-        "./app/**/*.{ts,tsx}",
-        "./src/**/*.{ts,tsx}",
-        "node_modules/preline/dist/*.js"
-    ],
-    prefix: "",
-    theme: {
-        extend: {
-            colors: {
-                primary: '#2B7516',
-                secondary: '#082503',
-            },
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    extend: {
+      colors: {
+        // màu chữ
+        primary: '#2B7516',
+        secondary: '#082503',
 
-            container: {
-                center: true,
-                padding: {
-                    DEFAULT: '1rem',
-                    sm: '2rem',
-                    lg: '4rem',
-                    xl: '5rem',
-                    '2xl': '6rem',
-                }
-            },
-            fontFamily: {
-                poppins: ["Poppins", "sans-serif"],
-                lora: ["lora"],
-                serif_text: ["DM Serif Text"],
-                inter: ["Inter", "sans-serif"]
-            },
-            screens: {
-                sm: '640px',
-                md: '760px',
-                lg: '960px',
-                xl: '1200px'
-            },
-            backgroundImage: {
-                'hero': "url('/src/assets/img/hero/bg.png')",
-            },
+        // Màu background
+        'main-bg': '#FFFFFF',
+        'soft-bg': '#515A3C',
+        // màu này là màu xanh lá ở account
+        'dark-bg': '#344F1F',
+        // màu xanh lá đậm ở landing page
+        'hightlight': '#C2A68C',
+      },
+
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
         },
+      },
+
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        lora: ["lora"],
+        serif_text: ["DM Serif Text"],
+        inter: ["Inter", "sans-serif"]
+      },
+
     },
-    plugins: [],
+  },
+  plugins: [],
 };
